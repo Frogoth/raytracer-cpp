b28168f8eeea81bd5bfac17a10ab81717519e8f7
Frogoth <tom.rudolf@epitech.eu>
2025-04-09 12:33:02 +0200

    style: remove useless includes + restyle includes

--- main.hpp
+++ main.hpp
@@ -6,15 +6,16 @@
 */
 
 #pragma once
-    #include <iostream>
-    #include <fstream>
-    #include <thread>
-    #include <vector>
-    #include <list>
-    #include <array>
-    #include <string>
-    #include <ostream>
-    #include <memory>
-    #include <cmath>
-    #include <limits>
-    #include <libconfig.h++>
+
+#include <iostream>
+#include <fstream>
+#include <thread>
+#include <vector>
+#include <list>
+#include <array>
+#include <string>
+#include <ostream>
+#include <memory>
+#include <cmath>
+#include <limits>
+#include <libconfig.h++>
